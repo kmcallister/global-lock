ded9cc234b10f93580d2a7cceb0135dfb3075b8f
Keegan McAllister <mcallister.keegan@gmail.com>
2011-11-02 00:23:38 -0400

    better uniquify C symbol names

--- System/GlobalLock/Internal.hs
+++ System/GlobalLock/Internal.hs
@@ -9,28 +9,34 @@ import Foreign.C
 import Control.Monad
 import Control.Concurrent.MVar
 
-{- Importing c_get_global_ptr with 'unsafe' decreases locking latency
-   by about 50%.  It's okay because that function just reads a C
-   static global.
+{- Importing c_get_global with 'unsafe' decreases locking latency by
+   about 50%.  It's okay because that function just reads a C static
+   global.
 
-   c_set_global_ptr must not be imported 'unsafe' because it uses GCC
+   c_set_global must not be imported 'unsafe' because it uses GCC
    atomic-operation builtins which, in the worst case, might call a
    blocking library function. -}
 
-foreign import ccall unsafe c_get_global_ptr :: IO (Ptr ())
-foreign import ccall        c_set_global_ptr :: Ptr () -> IO CInt
+{- If you are copying this file to your own Haskell project, see the
+   note in cbits/global.c regarding naming. -}
+
+foreign import ccall unsafe "hs_globalzmlock_get_global"
+    c_get_global :: IO (Ptr ())
+
+foreign import ccall "hs_globalzmlock_set_global"
+    c_set_global :: Ptr () -> IO CInt
 
 set :: IO ()
 set = do
     mv  <- newMVar ()
     ptr <- newStablePtr mv
-    ret <- c_set_global_ptr (castStablePtrToPtr ptr)
+    ret <- c_set_global (castStablePtrToPtr ptr)
     when (ret == 0) $
         freeStablePtr ptr
 
 get :: IO (MVar ())
 get = do
-    p <- c_get_global_ptr
+    p <- c_get_global
     if p /= nullPtr
         then deRefStablePtr (castPtrToStablePtr p)
         else set >> get
--- cbits/global.c
+++ cbits/global.c
@@ -7,12 +7,19 @@
 
 static void* global = 0;
 
+/*
+    If you are copying this file to your own Haskell project, you should
+    probably change these function names.  I suggest replacing 'globalzmlock'
+    with your package's z-encoded name, following
+    <http://hackage.haskell.org/trac/ghc/wiki/Commentary/Compiler/SymbolNames>
+*/
+
 // Imported 'unsafe' in Haskell code.  Must not block!
-void* c_get_global_ptr(void) {
+void* hs_globalzmlock_get_global(void) {
     return global;
 }
 
-int c_set_global_ptr(void* new_global) {
+int hs_globalzmlock_set_global(void* new_global) {
     // Set 'global', if it was previously zero.
     void* old = __sync_val_compare_and_swap(&global, 0, new_global);
 
